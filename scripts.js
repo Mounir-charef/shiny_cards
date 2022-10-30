6adedec2ae2672003f39cd763880231effa0550c
Mounir <polomnr1@gmail.com>
2022-10-30 14:04:32 +0100

    Done

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.getElementById("cards").onmousemove = e => {
+    for (const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+
+        card.style.setProperty("--mouse-x", `${x}px`)
+        card.style.setProperty("--mouse-y", `${y}px`)
+    }
+
+}
